dad62b7071967c3ca90f09d856f735907577dccd
Роман Кононов <kononovri@gmail.com>
2021-11-21 13:07:04 +0300

    1.01

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -12,9 +12,7 @@ namespace Lab4
         {
             for (int i = 0; i < 10; i++)
             {
-                if (i == 5)
-                    continue;
-                Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
+                               Console.WriteLine("Квадрат числа {0} равен {1}", i, i * i);
             }
                 Console.ReadKey();
         }
