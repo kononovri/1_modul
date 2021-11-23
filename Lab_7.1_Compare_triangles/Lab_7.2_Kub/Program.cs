78b7163acd5b999c96eab641a7a536251e5f8bde
Роман Кононов <kononovri@gmail.com>
2021-11-23 23:38:49 +0300

    1.0

---
+++ Lab_7.1_Compare_triangles/Lab_7.2_Kub/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7._2_Kub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Kub(a, out double V, out double S);
+            if (a > 0)
+            {
+                Console.WriteLine("Объем куба равен {0}", V);
+                Console.WriteLine("Площадь поверхности куба равна {0}", S);
+                Console.ReadKey();
+            }
+            else Console.WriteLine("Вы ввели некорректную длину");
+            Console.ReadKey();
+        }
+        static void Kub(int a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = 6 * a * a;
+        }
+
+    }
+
+}
---
+++ Lab_7.2_Kub/Lab_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int V, S;
+            Kub(a, V, S);
+            Console.WriteLine("Объем куба равен {0:F2}", V);
+            Console.WriteLine("Площадь поверхности куба равна {0:F2}", S);
+            Console.ReadKey();
+        }
+    }
+    static void Kub(int a, out int V, out int S)
+    {
+        int V = Math.Pow(a, 3);
+        int S = 6 * a * a;
+    }
