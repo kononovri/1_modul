98b6f814dab531b8bb1e7d7eb423264363c5df3d
Роман Кононов <kononovri@gmail.com>
2021-12-02 00:15:40 +0300

    1.0

---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cet();
+            cat.name = "Мурка";
+            cat.color = "Черный";
+            cat.age = 1;
+            Console.WriteLine(cat.name);
+            Console.WriteLine(cat.color);
+            Console.ReadKey();
+        }
+    }
+    class Cat
+    {
+        private string name;
+        public string color;
+        private int age;
+
+        public int Age
+        {
+            set
+            {
+                if (value>0)
+                {
+                    age = value;
+
+                }
+            }
+        
+            public int GetAge()
+        {
+            return age;
+        }
+        void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
---
+++ Lab_10.1/Lab_10.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите через пробел величину угла в градусах, минутах и секундах:");
+            int[] arrayС1 = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            if (Math.Abs(arrayС1[0]) > 360 || arrayС1[1] > 60 || arrayС1[2] > 60)
+            {
+                Console.WriteLine("Ошибка! Введены некорректные величины");
+            }
+            else
+            {
+                Corner corner1 = new Corner(arrayС1[0], arrayС1[1], arrayС1[2]);
+                corner1.ToRadians();
+            }
+            Console.ReadKey();
+        }
+    }
+    class Corner
+    {
+        readonly int Grade;
+        readonly int Min;
+        readonly int Sec;
+        public Corner(int Grade, int Min, int Sec)
+        {
+            this.Grade = Grade;
+            this.Min = Min;
+            this.Sec = Sec;
+        }
+        public void ToRadians()
+        {
+            if (Grade >= 0)
+            {
+                double Radians = (Grade + Min / 60 + Sec / 3600) * (Math.PI) / 180;
+                Console.WriteLine("Величина указанного угла составляет: {0:F2} рад", Radians);
+            }
+            else
+            {
+                double Radians = (Grade - Min / 60 - Sec / 3600) * (Math.PI) / 180;
+                Console.WriteLine("Величина указанного угла составляет: {0:F2} рад", Radians);
+            }
+
+        }
+    }
+}
