f8dd5d4902d5838123f66f16a1d1cf5ab66beac3
Роман Кононов <kononovri@gmail.com>
2021-12-12 11:49:00 +0300

    1.0

---
+++ Lab13/Lab13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(1, 2, 3);
+            ReactangularTriangle rectangulartriangle = new ReactangularTriangle();
+            rectangulartriangle.GetArea;
+        }
+    }
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+
+        }
+        public double GetArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+
+    }
+    class ReactangularTriangle : Triangle
+    {
+        public ReactangularTriangle(double a, double b)
+                : base(a, b, Math.Sqrt(a * a + b * b));
+
+    }
+}
---
+++ Lab_13.1/Lab_13.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_13._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiBuilding multibuilding = new MultiBuilding("Ул. Полевая, 27", 30, 9, 12, 20);
+            multibuilding.Print();
+            Console.ReadKey();
+        }
+    }
+    class Building
+    {
+        public string Adress { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Building(string a, double l, double w, double h)
+        {
+            Adress = a;
+            Length = l;
+            Width = w;
+            Height = h;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Адрес здания: {0}", Adress);
+            Console.WriteLine("Длина здания: {0} м", Length);
+            Console.WriteLine("Ширина здания: {0} м", Width);
+            Console.WriteLine("Высота здания: {0} м", Height);
+        }
+    }
+    sealed class MultiBuilding : Building
+    {
+        public double Floors { get; set; }
+        public MultiBuilding(string a, double l, double w, double h, double f)
+        : base(a, l, w, h)
+        {
+            Floors = f;
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine("Этажей в здании: {0}", Floors);
+        }
+    }
+
+}
