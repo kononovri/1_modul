c70baaf58dc3dbfdd1f229d6ec3608e8fa1865da
Роман Кононов <kononovri@gmail.com>
2021-12-12 14:06:19 +0300

    1.0

---
+++ Lab_15.1/Lab_15.1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_15._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кошка");
+            cat.ShowInfo();
+            Dog dog = new Dog("Собака");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine("Я - {0}!", Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string name;
+        public Cat(string name)
+            : base(name)
+        {
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        public Dog(string name)
+            : base(name)
+        {
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав!");
+        }
+    }
+}
